afd70616818ba51a821ca3844ef30caa2e6ebd5f
Alexander <7911562+av-pavlov@users.noreply.github.com>
2019-02-03 18:48:45 +0900

    init

---
+++ guess/guess.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <clocale>
+
+using namespace std;
+
+int main(){
+    setlocale(LC_ALL, "ru");
+
+    cout << "Загадайте число от 0 до 1024, я попытаюсь его угадать.\n"
+         << "Отвечайте 1, если загаданное число больше,\n"
+         << "-1, если меньше, и 0, если я угадаю.\n";
+
+    int l = 0, r = 1024, c, answer;
+
+    c = (l+r) / 2;
+    cout << c << "? ";
+    cin >> answer;
+
+    if (answer == 1) {
+        l = c;
+    }
+    if (answer == -1) {
+        r = c;
+    }
+
+    c = (l+r) / 2;
+    cout << c << "? ";
+    cin >> answer;
+
+    return 0;
+}
