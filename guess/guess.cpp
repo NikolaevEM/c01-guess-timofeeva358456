761de5e076ef4d2428574cd07b920fd4fd437fa6
timofeeva358456 <ayyyna.timofeeva@mail.ru>
2019-02-07 15:26:19 +0900

    от 0 до 1024 не включительноЭ

--- guess/guess.cpp
+++ guess/guess.cpp
@@ -6,36 +6,53 @@ using namespace std;
 int main(){
     setlocale(LC_ALL, "ru"); // дл€ русских букв в консоли
 
-	printf("«агадайте число от 0 до 1024, € попытаюсь его угадать.\n");
+	printf("«агадайте число от 0 до 1024 не включительно, € попытаюсь его угадать.\n");
+	//если что игра угадывает число от 0 до 1023, а 1024 угадывает с 11 попытки
 	printf("ќтвечайте 1, если загаданное число больше,\n");
     printf("-1, если меньше, и 0, если € угадаю.\n");
 
-	int l = 0, r = 1024, s = 1, c, answer; // s = счетчик попыток
-	c = (l + r) / 2;
-	printf("ѕопытка %d : %d?", s, c);
-	scanf("%d", &answer);
+	int l = 0, r = 1024, s = 1, c = 1024, answer = 1; // s = счетчик попыток
 
 	while (answer!=0)
 	{
-		switch (answer)
-		{
-		case 1:
-			l = c;
-			break;
-
-		case -1:
-			r = c;
-			break;
-
-		case 0:
-			printf("я угадал");
-			break;
-		}
-		s++;
 		c = (l + r) / 2;
 		printf("ѕопытка %d : %d?", s, c);
 		scanf("%d", &answer);
 
+		switch (answer)
+		{
+		case 1:
+			//if дл€ того, чтобы угадывал за 11 попыток 1024, тк игра не угадывает 1024 за 10 попыток
+		//	if (++c == r)
+		//	{
+		//		c = r;
+		//		answer = 0;
+		//	}
+		//	else 
+				l = c;
+			break;
+		
+		case -1:
+			if (--c == r)
+			{
+	 		c = l;
+				answer = 0;
+			}
+			else
+				r = c;
+			break;
+
+		case 0:
+			break;
+		default:
+			s--;
+			printf("Ќеправильно\n");
+			break;
+		}
+		s++;
+		
 	}
+
+	printf("я угадал твоЄ загаданное число, это %d ", c);
     return 0;
 }
